658d1ff8c9fa50d14031c0607211eb1fa1f4e6df
SurajPa05 <techongamer118@gmail.com>
2025-05-13 19:27:37 +0530

    Base Structure

---
+++ XorCypher.py
@@ -0,0 +1,15 @@
+key = ''
+
+#encryption algorithm
+def encrypter():
+    print("encryptes the text")
+
+#decryption algorithm
+def decrypter():
+    print("decryptes the text")
+
+#generates random key
+def Generate_Key():
+    print("Key genereator")
+
+
